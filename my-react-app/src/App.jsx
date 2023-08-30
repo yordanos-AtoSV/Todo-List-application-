8207eb224dea807c3fd835edc5492443f8e9f466
yordanos yadessa <yordanosyadessa2019@gmail.com>
2023-08-30 11:31:35 +0300

    first commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className={card}>
         <button onClick={counter}>
-          count isss {count}
+          count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
